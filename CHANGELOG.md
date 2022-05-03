5a810d5bb3f49390bc1647c7c45233d915a5c74f
Ben Parizek <ben.parizek@gmail.com>
2022-05-03 16:10:28 -0400

    Initial commit c4

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# Changelog
+
+## 4.0.0 - UNRELEASED
+
+
---
+++ src/SproutCampaigns.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace BarrelStrength\SproutCampaigns;
+
+use BarrelStrength\Sprout\campaigns\CampaignsModule;
+use BarrelStrength\Sprout\core\db\InstallHelper;
+use BarrelStrength\Sprout\core\db\SproutPluginMigrationInterface;
+use BarrelStrength\Sprout\core\db\SproutPluginMigrator;
+use BarrelStrength\Sprout\core\editions\Edition;
+use BarrelStrength\Sprout\core\modules\Modules;
+use BarrelStrength\Sprout\mailer\MailerModule;
+use Craft;
+use craft\base\Plugin;
+use craft\db\MigrationManager;
+use craft\errors\MigrationException;
+use craft\events\RegisterComponentTypesEvent;
+use craft\helpers\UrlHelper;
+use yii\base\Event;
+use yii\base\InvalidConfigException;
+
+class SproutCampaigns extends Plugin implements SproutPluginMigrationInterface
+{
+    public const EDITION_LITE = 'lite';
+    public const EDITION_PRO = 'pro';
+
+//    public $minVersionRequired = '1.5.3';
+
+    /**
+     * @inheritDoc
+     */
+    public static function editions(): array
+    {
+        return [
+            self::EDITION_LITE,
+            self::EDITION_PRO,
+        ];
+    }
+
+    public static function getSchemaDependencies(): array
+    {
+        return [
+            CampaignsModule::class,
+            MailerModule::class,
+        ];
+    }
+
+    /**
+     * @throws InvalidConfigException
+     */
+    public function getMigrator(): MigrationManager
+    {
+        return SproutPluginMigrator::make($this);
+    }
+
+    public string $schemaVersion = '0.0.1';
+
+    public function init(): void
+    {
+        parent::init();
+
+        Event::on(
+            Modules::class,
+            Modules::EVENT_REGISTER_SPROUT_AVAILABLE_MODULES,
+            function(RegisterComponentTypesEvent $event) {
+                $event->types[] = CampaignsModule::class;
+            }
+        );
+
+        $this->instantiateSproutModules();
+        $this->grantModuleEditions();
+    }
+
+    protected function instantiateSproutModules(): void
+    {
+        CampaignsModule::isEnabled() && CampaignsModule::getInstance();
+    }
+
+    protected function grantModuleEditions(): void
+    {
+        if ($this->edition === self::EDITION_PRO) {
+            CampaignsModule::isEnabled() && CampaignsModule::getInstance()->grantEdition(Edition::PRO);
+        }
+    }
+
+    /**
+     * @throws MigrationException
+     */
+    protected function afterInstall(): void
+    {
+        InstallHelper::runInstallMigrations($this);
+
+        if (Craft::$app->getRequest()->getIsConsoleRequest()) {
+            return;
+        }
+
+        // Redirect to welcome page
+        $url = UrlHelper::cpUrl('sprout/welcome/campaigns');
+        Craft::$app->getResponse()->redirect($url)->send();
+    }
+
+    /**
+     * @throws MigrationException
+     * @throws InvalidConfigException
+     */
+    protected function beforeUninstall(): void
+    {
+        InstallHelper::runUninstallMigrations($this);
+    }
+}
